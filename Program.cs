ee99ac57b9e6744210ed96ba793d2fbb5f2e664e
CarlosEduSL <carloslima.adr122@gmail.com>
2024-12-10 23:26:55 -0300

    Treinando projeto de treinar a lógica finalizado

--- Program.cs
+++ Program.cs
@@ -4,20 +4,52 @@ class Program
 {
     static void Main(string[] args)
     {
-        string nomeDoHeroi = "PVP FM";
+        string nomeDoPlayer = "PVP FM";
         int level = 4000;
+        string elo = "";
 
-       Console.WriteLine("Coloque seu nickname");
-        nomeDoHeroi = Console.ReadLine();
+        Console.WriteLine("Coloque seu nickname");
+        nomeDoPlayer = Console.ReadLine();
 
         Console.WriteLine("Coloque qual é seu Level");
-        level = Console.ReadLine();
+        level = int.Parse(Console.ReadLine());
+
+        if (level < 1000){
+            elo = "Ferro";
+        }
+
+        else if (level <= 2000){
+            elo = "Bronze";
+        }
+
+        else if (level <= 5000){
+            elo = "Prata";
+        }
+
+        else if (level <= 7000){
+            elo = "Ouro";
+        }
+
+        else if (level <= 8000){
+            elo = "Platina";
+        }
+
+        else if (level <= 9000){
+            elo = "Ascendente";
+        }
+
+        else if (level <= 10000){
+            elo = "Imortal";
+        }
+
+        else
+            elo = "Radiante";
 
 
+
+
+
+
+        Console.WriteLine($"O Player de nick {nomeDoPlayer} está no ranking atual {elo}");
     }
-
-     
-
-
-
 }
