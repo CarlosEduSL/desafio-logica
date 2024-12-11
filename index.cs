caef4537d05d402b82ba967a8ecc86fd76af833b
CarlosEduSL <carloslima.adr122@gmail.com>
2024-12-10 23:02:08 -0300

    criando .NET

---
+++ index.cs
@@ -0,0 +1,23 @@
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string nomeDoHeroi = "PVP FM";
+        int level = 4000;
+
+       Console.WriteLine("Coloque seu nickname");
+        nomeDoHeroi = Console.ReadLine();
+
+        Console.WriteLine("Coloque qual é seu Level");
+        level = Console.ReadLine();
+
+
+    }
+
+     
+
+
+
+}
